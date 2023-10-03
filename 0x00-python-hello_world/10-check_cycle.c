5596648580cb5246a62b7b8ef8c5909cee0b3421
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-03 09:20:54 +0100

    new line in write

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,4 +4,18 @@
 
 int check_cycle(listint_t *list)
 {
+	listint_t *ptr1, *ptr2;
+
+	if (list == NULL)
+		return (0);
+	ptr1 = list;
+	ptr2 = ptr1->next;
+	while (ptr1 && ptr2)
+	{
+		ptr1 = ptr2->next;
+		ptr2 = ptr1->next;
+		if (ptr1 == list || ptr2 == list)
+			return (1);
+	}
+	return (0);
 }
