a44a0e9fb640909e16b9fa610c5737411979e958
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-04 11:41:46 +0100

    python style

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,9 +2,9 @@
 def remove_char_at(str, n):
     new_str = ""
     i = 0
-    while i < len(str):
+    for ch in str:
         if i == n:
             continue
         else:
-            new_str[i] = str[i]
+            new_str += ch
         i += 1
