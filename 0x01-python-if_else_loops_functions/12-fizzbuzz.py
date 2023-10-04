e96a0f4c55e633800cc8c0df33b4645603b4fff5
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-04 03:56:10 +0100

    get done

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{}" .format(i), end="")
+        if i != 100
+            print(" "), end="")
+    print("")
