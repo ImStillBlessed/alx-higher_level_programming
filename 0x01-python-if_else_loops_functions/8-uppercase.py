3f0c6e49ed06becb9c62056f0d323578c1aa5733
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-04 03:23:01 +0100

    get done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for ch in str:
         if 96 < ord(ch) < 123:
             ch = ord(ch) - 32
-            print("{}" .format(chr(ch)), end="")
-        else:
-            print("{}" .format(chr(ch)), end="")
+        print("{}" .format(chr(ch)), end="")
     print("\n")
 # change the letters to upper first then, print the string 
