a7ec44fd3f1a287730cbe387cc1b274ea15cac8b
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-04 03:18:40 +0100

    get

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,11 @@
 def uppercase(str):
     for ch in str:
         if 64 < ord(ch) < 91:
-            continue
+            print("{}" .format(chr(ch), end=""))
         elif 96 < ord(ch) < 123:
             ch = ord(ch) - 32
+            print("{}" .format(chr(ch)), end="")
+        else:
             print("{}" .format(chr(ch)), end="")
     print("\n")
 # change the letters to upper first then, print the string 
