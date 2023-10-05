24537d2304a157286d5314a7bf2c19b6aebd91e0
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-05 21:14:06 +0100

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import * as name
-    while(name):
-        print("{}" .format(name))
-
+    import hidden_4
+    for name in dir(hidden_4):
+        if not name.startwith("__"):
+            print("{}" .format(name))
