b02cc74cc54272e8d98c77bafd0cf9044c7b607c
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-07 20:03:39 +0100

    finished

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i, j = 0, 0
+    for i in matrix:
+        for j in matrix[i]:
+            print("{}" .format(j), end="")
+        print("")
