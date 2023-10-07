4ba64b940cdd17e10a6cfa484eea47bda3cb4c4c
blessed <eprince1800@gmail.co>
2023-10-07 09:07:18 -0700

    naked run no tess

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    j = 0
+    for i in my_list:
+        new_list[j] = i
+        j++
+    new_list[idx] = element
+    return new_list
