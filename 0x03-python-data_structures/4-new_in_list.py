482d7edd5df05f37a3d9fce31b0db6935aa1a4fa
blessed <eprince1800@gmail.co>
2023-10-07 09:11:59 -0700

    fixed the functin using append instead of a loop

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,7 @@ def new_in_list(my_list, idx, element):
     new_list = []
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
-    j = 0
     for i in my_list:
-        new_list[j] = i
-        j++
+        new_list.append(i)
     new_list[idx] = element
     return new_list
