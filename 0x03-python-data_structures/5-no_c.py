33726f58b68812f8145d1c4dfc3b5b6e92be7038
blessed <eprince1800@gmail.co>
2023-10-07 09:28:39 -0700

    fixed the no c functions

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for ch in my_string:
-        if ch == "c" or ch == "C":
-            continue
-        else:
+        if ch != "c" or ch != "C":
             new_string += ch
     return new_string
