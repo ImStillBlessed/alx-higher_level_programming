cf660baaab82e259c9a1462c0fc1207a2324a78c
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-09 15:40:19 +0100

    Trye and False

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,9 +2,10 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return None
+    i = 0
     while i < len(my_list):
         if my_list[i] % 2 == 0:
-            my_list[i] == True
+            my_list[i] = True
         else:
-            my_list[i] == False
+            my_list[i] = False
     return my_list
