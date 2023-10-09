d83d38936de4806838acb03ed047c41769b1df97
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-09 15:49:02 +0100

    new_list_

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if len(my_list) <= idx or idx < 0:
+        return my_list
+    i = 0
+    new_list = []
+    while i < len(my_list):
+        if i != idx:
+            new_list[i] = my_list[i]
+        i += 1
+    return new_list
