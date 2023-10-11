1f66a37fd582eb9ecf8469ddbfa53b526f9c63d3
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-11 16:49:40 +0100

    second try

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x**2 for x in matrix[y]] for y in matrix]
+    new_matrix = [[x**2 for x in y] for y in matrix]
     return new_matrix
