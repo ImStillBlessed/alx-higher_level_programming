6f0f06c6bd5fcc43928b20de902b6f74787561ff
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-11 17:40:57 +0100

    forgot to make it print not return the values

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictiknary[key]}")
+        print(f"{key}: {a_dictionary[key]}")
