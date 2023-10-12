bdf70cebc0c8c202d2c2d507c11ac00f9ec91132
oigbochie blessed <eprince1800@gmail.com>
2023-10-12 06:45:26 +0000

    using del instead of pop for dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return None
-
     keys_del = [key for key, val in a_dictionary.items() if val == value]
 
     for key in keys_del:
-        a_dictionary.pop(key)
+        del a_dictionary[key]
 
     return a_dictionary
