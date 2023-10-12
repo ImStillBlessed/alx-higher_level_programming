bc2748585e1826331ce5d71916c5be8caeef8e2b
oigbochie blessed <eprince1800@gmail.com>
2023-10-12 06:30:39 +0000

    made the code 3 lines long

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_mat = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
-    return new_mat
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
