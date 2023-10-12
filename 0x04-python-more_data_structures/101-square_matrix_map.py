5391baba2edccab51d4547ca5e0cf31c0f6571fc
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-12 07:15:36 +0100

    f

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,5 +2,5 @@
 def square_matrix_map(matrix=[]):
     if not matrix:
         return None
-    new_mat = [for y in matrix list(map(lambda x: x**2, for x in y))]
+    new_mat = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
     return new_mat
