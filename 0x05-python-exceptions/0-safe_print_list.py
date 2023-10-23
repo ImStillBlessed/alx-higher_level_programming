dad87304f96421b41eaad764fe4e7b6d728f5366
Blessed Oigbochie <eprince1800@gmail.com>
2023-10-23 13:45:48 +0100

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i, item in enumerate(my_list):
-            while i <= x:
-                print(item, end="")
-                if i != x:
-                    print(", ", end="")
+            if !(i <= x):
+                break
+            print(item, end="")
+            if i != x:
+                print(", ", end="")
         print("")
     except Exception as err:
         print(err)
