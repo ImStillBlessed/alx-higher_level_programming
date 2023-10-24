1785fc033a85d38bc8a8ee5877564e23f973e2ff
Oigbochie Blessed <eprince1800@gmail.com>
2023-10-24 17:20:11 +0100

    i really don't know how to do this google documentation but...

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This class defines a square
+
+    Attributes:
+        __size (int): This is the size f the square
+    """
     def __inti__(self):
+        """
+        the constructor for the square class
+
+        Args:
+            size (int): the size of the square.
+        """
         self.__size = size
