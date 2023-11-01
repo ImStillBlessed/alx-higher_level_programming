e81d07a29c4059364d7d9887b7d599380785db77
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-01 19:59:23 +0100

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,5 +5,17 @@ Author:
 """
 
 
-class LockedClass():
+class LockedClass:
+    """
+        This class defines a class first name on5ly
+            any other objects or class is rejected
 
+        Attributes:
+            none
+    """
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            error = "'LockedClass' object has no attribute '{}'".format(name)
+            raise AttributeError(error)
+        else:
+            super().__setattr__(name, value)
