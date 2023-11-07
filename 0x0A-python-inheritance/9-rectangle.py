7921f8e1fcf5ac65a0df8a4a87e481ddd12e89cd
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-07 03:45:59 +0100

    9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Author:
+    Oigbochie Blessed
+"""
+
+
+class BaseGeometry():
+    """
+    This class defines basic geometry
+    Attributes:
+        area(): finds the area of the shape.
+    Raises:
+        Exception as the area is not implemented.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This function validates if name is an integer.
+        Args:
+            name (str): the value name.
+            value (int): rhe value assigned to the name.
+        Raises:
+            TypeError: if value is not type int.
+            ValueError: if value is less < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.name = name
+        self.value = value
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class defines a rectangle shape
+    Attributes:
+        height (int): the height of the rectangle.
+        width (int): the width of the rectangle.
+    """
+    def __init__(self, width, height):
+        """
+        initializes the rectangle class with width and height
+        Args:
+            height (int): height.
+            width (int): width
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle]: {}/{}".format(self.__width, self.__height)
