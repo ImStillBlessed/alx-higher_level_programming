5800c3ca1f7bc7a01bf9eb18aac7b94cec44cae4
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-08 22:06:15 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,11 +5,16 @@ Author:
 """
 
 
+import sys
 import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item(argc, argv):
+if __name__== "__main__":
     """
-    This script takes
+    This module loads content from json file and writes i
+    to a new file in json.
+    """
+    arg_list = sys.argv[1:]
+    save_to_json_file(arg_list, "add_item.json")
