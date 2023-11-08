4ad33d1e1620acef6d3dcfb9ec32275cb934e0dd
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-08 08:37:42 +0100

    tast 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Author:
+    Oigbochie Blessed
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function writes inout text to a file
+    Args:
+        filename (str): the name of the file to wfite to.
+        text (str, int): the content to write to the file.
+    
+    the file is created if it doesnt exist and overwritten if it has content.
+    """
+    with open(filename, "w", encoding="UTF-8") as a_file:
+        return a_file.write(str(text))
