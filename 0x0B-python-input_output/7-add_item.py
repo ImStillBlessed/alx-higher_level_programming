9d139a1bd78f132a26ec27942383e64fec640ce9
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-08 22:14:25 +0100

    finished task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,15 +6,20 @@ Author:
 
 
 import sys
-import json
+import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     """
     This module loads content from json file and writes i
     to a new file in json.
     """
-    arg_list = sys.argv[1:]
-    save_to_json_file(arg_list, "add_item.json")
+    filename = "add_item.json"
+    if os.path.exists(filename):
+        arg_list = load_from_json_file(filename)
+    else:
+        arg_list = []
+    arg_list.extend(sys.argv[1:])
+    save_to_json_file(arg_list, filename)
