85002f1cd41be822431c406b523b4d86571efedb
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-08 13:53:08 +0100

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -10,4 +10,9 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    This function returns a string in json format.
+    Args:
+        my_obj (str): the objevt to read as json.
+    """
     return json.dumps(my_obj)
