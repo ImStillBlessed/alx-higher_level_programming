424dc0a9b854f5cf498bde1c42588b801b874d95
Blessed Oigbochie <eprince1800@gmail.com>
2023-11-08 13:59:28 +0100

    json 2

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Author:
+    Oigbochie Blessed
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This function prints a string represented in json format
+    Args:
+        my_str (str): the string to convert
+    """
+    return json.dumps(my_str)
