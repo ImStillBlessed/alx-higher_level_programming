f5577b95658ab847143c53400c3c1080b4439323
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-12 22:19:19 +0100

    task 2

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- creates a database hbtn_0c_0
+-- no errors even if it exixts
 CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
 -- Deletes a database from the server
+-- No erros even if it doesn't exist
 DROP DATABASE [IF EXISTS] hbtn_0c_0
