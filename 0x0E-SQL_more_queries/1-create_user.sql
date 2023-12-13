dc9bb7f959b649f5ca583f9bab3bffd279a9be61
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-13 19:01:29 +0100

    task 1 and 2 finally

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create a user with all the privileges of root user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON '*' TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
