74a9bf39550484434a35ca7d5f065819169128aa
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-13 19:16:53 +0100

    task 4 and 5

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- creates a table where id is defualt 1
-CREATE TABLE IF NOT EXISTS 'id_not_null' (
-    id INT DEFUALT 1,
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
     name VARCHAR(256)
 )
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- CREATES TABLE WITH id default 1 but must be unique
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1
+    name VARCHAR(256)
+    UNIQUE (id)
+);
