fd99b437a2c7cbd04bf2aab66c792ced110a1f1a
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-13 04:25:09 -0800

    final changes for mandatory

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,7 @@
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(
 	id INT,
 	name VARCHAR(256),
-	score INT
-);
+	score INT);
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table(id, name, score) VALUES (3, 'Bob', 14);
