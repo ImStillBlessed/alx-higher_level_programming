0aee0b7fb8a0c43f64fe06cf26c5ee28d7cb7a78
Blessed Oigbochie <eprince1800@gmail.com>
2023-12-13 18:40:17 +0100

    corrections for the task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- show all privileges granted to user_0d_1 and user_0d_2
-SHOW GRANTS 'user_0d_1'@'localhost', 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
