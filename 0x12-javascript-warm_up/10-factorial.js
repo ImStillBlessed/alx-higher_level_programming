f7b9960b79b1d388239ea6491cd41d3517a951af
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-08 21:52:06 +0100

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-let argument = Number(args[0])
+const argument = Number(args[0]);
 function factorial (num) {
   if (num <= 1 || isNaN(num)) {
     return (1);
   }
   return (num * factorial(num - 1));
-};
+}
 console.log(factorial(argument));
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-let size = Number(args[0])
+const size = Number(args[0]);
 if (size) {
   for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) {
       row += 'X';
-    };
+    }
     console.log(row);
-  };
+  }
 } else {
   console.log('Missing size');
-};
+}
