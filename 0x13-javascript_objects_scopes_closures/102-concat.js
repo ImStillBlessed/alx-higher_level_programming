135dbe57ca08ae2cb8278921f56a902891dbc413
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-09 20:44:56 +0100

    task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,29 +1,39 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const args = process.argv.slice(2);
 
-fs.readFile(args[0], 'utf8', (err, dataA) => {
-  if (err) {
-    console.error(err.message);
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <fileA> <fileB> <fileC>');
+  process.exit(1);
+}
+
+const [, , fileA, fileB, fileC] = process.argv;
+
+// Read the content of fileA
+fs.readFile(fileA, 'utf8', (errA, dataA) => {
+  if (errA) {
+    console.error(errA.message);
     process.exit(1);
   }
 
-  fs.readFile(args[1], 'utf8', (err, dataB) => {
-    if (err) {
-      console.error(err.message);
+  // Read the content of fileB
+  fs.readFile(fileB, 'utf8', (errB, dataB) => {
+    if (errB) {
+      console.error(errB.message);
       process.exit(1);
     }
 
+    // Concatenate the content of fileA and fileB
     const concatenatedContent = dataA + dataB;
 
-    fs.writeFile(args[2], concatenatedContent, (err) => {
-      if (err) {
-        console.error(err.message);
+    // Write the concatenated content to fileC
+    fs.writeFile(fileC, concatenatedContent, (errC) => {
+      if (errC) {
+        console.error(errC.message);
         process.exit(1);
       }
 
-      console.log(`The content of ${args[0]} and ${args[1]} has been concatenated to ${args[2]}`);
+      console.log(`The content of ${fileA} and ${fileB} has been concatenated to ${fileC}`);
     });
   });
 });
