931d237c2d916fbe2829b79838688a54d7a2ee28
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-10 13:24:37 +0100

    task 103 final task first summition

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+obj.incr = functon () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
