fb03dbdbefe49d84acbdb8c465e141a433d20f77
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-12 16:51:50 +0100

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this is a script that lists all states with state_name
+this is a script that lists all states and cities
 from the database hbtn_0e_0_usa:
 with injection protection.
 """
@@ -15,9 +15,16 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     state_name = argv[4]
-    sql_query = "SELECT id, name FROM states WHERE name = %s ORDER BY states.id ASC"
-    cur.execute(sql_query, (state_name,))
-    states_list = cur.fetchall()
-    for state in states_list:
-        print(state)
+
+
+    sql_query = "SELECT s.id, s.name, c.name \
+            FROM states AS s \
+            INNER JOIN cities AS c \
+            ON c.state_id = s.id \
+            ORDER BY s.id ASC"
+
+    cur.execute(sql_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     db.close()
