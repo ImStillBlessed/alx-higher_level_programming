03719a914be764c19deb9c92d35b83129807a071
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-12 15:21:45 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this is a script that lists all states from state_name
+from the database hbtn_0e_0_usa:
+
+Your script should take 4 arguments: mysql username, mysql password,
+database name and the state name.
+(no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    grant access to the db
+    """
+    db = MySQLdb.connect(host='localhost', user=argv[1], \
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    state_name = argv[4]
+    cur.execute("SELECT id, name FROM states \
+            WHERE name = %s ORDER BY states.id ASC", (state_name,))
+    states_list = cur.fetchall()
+    for state in states_list:
+        print(state)
+    db.close()
