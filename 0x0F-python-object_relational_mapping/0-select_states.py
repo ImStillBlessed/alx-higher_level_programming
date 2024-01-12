070055bf9422aff71fd3eef85d8cb34c3a28b330
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-12 10:08:48 +0100

    task 0 first

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this is a script that lists all states from the database hbtn_0e_0_usa:
+
+Your script should take 3 arguments: mysql username, mysql password and database name
+(no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost', user='root', passwd='root', db='hbtn_0e_0_usa')
+cur = db.cursor()
+
+cur.excecute("SELECT id, state FROM states ORDER BY id ASC")
+states = cur.fetchall()
+for state in states:
+    print(state)
