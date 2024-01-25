1361cab7fa7a10a94c90ae1933992b509a7b144b
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-25 08:01:33 -0800

    line fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-URL=$1
-SIZE=$(curl -sI "$URL" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-echo "$SIZE"
+# This script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+echo "$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')"
