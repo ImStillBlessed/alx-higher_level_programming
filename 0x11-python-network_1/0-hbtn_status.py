0123d8bf251aaed64dfe837677b57f5e53b7d3c5
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-26 20:09:50 +0100

    task 0 syntax correctoom

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        print(f"""Body response:$
-    - type: {type(html)}$
-    - content: {html}$
-    - utf8 content: {html.decode('utf8')}$""")
+        print(f"""Body response:
+    - type: {type(html)}
+    - content: {html}
+    - utf8 content: {html.decode('utf8')}""")
