4a77d286d269a607316a80473be74aebc8b1628f
Blessed Oigbochie <eprince1800@gmail.com>
2024-01-26 20:05:54 +0100

    task 0 - 2

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print(f"""Body response:$
+    - type: {type(html)}$
+    - content: {html}$
+    - utf8 content: {html.decode('utf8')}$""")
