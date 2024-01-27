f7e07f444cea43f6671995bb15e284aac4deda72
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-27 09:30:02 +0100

    task 7 corrections

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     response = requests.get(url)
-    print(response.text)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
+
