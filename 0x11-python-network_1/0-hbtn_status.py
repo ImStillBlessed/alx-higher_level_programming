85e2381109eee9cdbf6466d9a140152a69ac1830
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-27 08:23:57 +0100

    task 0 tabulation corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        print(f"""Body response:
-- type: {type(html)}
-- content: {html}
-- utf8 content: {html.decode('utf8')}""")
+        print(f"Body response:")
+        print(f"\t- type: {type(html)}")
+        print(f"\t- content: {html}")
+        print(f"\t- utf8 content: {html.decode('utf8')}")
