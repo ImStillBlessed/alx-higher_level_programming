761c00496b5bd794020884fcbd0b4679c6df4c46
Oigbochie Blessed <eprince1800@gmail.com>
2024-01-27 07:43:09 +0100

    task 3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 
+url = 'https://alx-intranet.hbtn.io/status'
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         print(f"""Body response:
-    - type: {type(html)}
-    - content: {html}
-    - utf8 content: {html.decode('utf8')}""")
+- type: {type(html)}
+- content: {html}
+- utf8 content: {html.decode('utf8')}""")
