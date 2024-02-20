ec725a8374bdd3b1f93036ec2b6022846753d6b3
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-20 17:53:00 +0100

    task  Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,11 @@ const url = process.argv[1];
 
 request(url, (error, response) => {
   console.error(error);
-  console.log('code:', response.statusCode); 
+  console.log('code: ', response.statusCode); 
+});
+
+request
+  .get(url)
+  .on('response', function(response) {
+    console.log('code: ', response.statusCode)
 });
