074d3200998b1db2f5db357fa2a48f9c46bcda92
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-20 19:58:27 +0100

    semistandard for ststus code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@ const request = require('request');
 
 const url = process.argv[1];
 
-request(url, function(error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    console.log("code:", response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
