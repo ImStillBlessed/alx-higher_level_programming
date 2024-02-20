07e17cee68c2e3f1b8acd3e7028ed8b7cdd692f3
Oigbochie Blessed <eprince1800@gmail.com>
2024-02-20 20:42:14 +0100

    changing the argument variable

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 
-if (process.argv.length !== 3) {
+if (process.argv.length !== 4) {
   console.error('Usage: ./0-readme.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
-const data = process.argv[2];
-const filePath = process.argv[1];
+const data = process.argv[3];
+const filePath = process.argv[2];
 
 fs.writeFile(filePath, data, err => {
   if (err) {
